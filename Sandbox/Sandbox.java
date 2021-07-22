61eb76ba026c92dd1279d7dcdd52ba44527ddca5
ramdanks <ramadhan.kalih@ui.ac.id>
2021-07-22 21:52:35 +0700

    add test sample

--- Sandbox/Sandbox.java
+++ Sandbox/Sandbox.java
@@ -6,7 +6,7 @@ final class Sandbox
 {
     public static void main(String[] args)
     {
-        NetlabUT[] unitList = { new Formatting() };
+        NetlabUT[] unitList = { new StringTest(), new ArrayTest() };
 
         GradeForm gf = new GradeForm("Modul 1 - Praktikum Pemrograman Berorientasi Obyek", unitList);
         gf.setVisible(true);
