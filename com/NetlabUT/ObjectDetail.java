ce79386277dc0a64c2e8485f70469c394bec0ce2
ramdanks <ramadhan.kalih@ui.ac.id>
2021-08-08 12:40:57 +0700

    new object + throwable detail

--- com/NetlabUT/ObjectDetail.java
+++ com/NetlabUT/ObjectDetail.java
@@ -4,9 +4,19 @@ import javax.swing.*;
 
 public class ObjectDetail extends JFrame
 {
+    private JPanel mainPanel;
+    private JTextArea textString;
+    private JLabel labelClass;
+
     public ObjectDetail(Object object)
     {
+        labelClass.setText(object.getClass().getName());
+        textString.setText(object.toString());
+
         setTitle(Profile.getObjectIdentifierString(object));
+        setContentPane(mainPanel);
+        setMinimumSize(mainPanel.getMinimumSize());
+        setSize(mainPanel.getMinimumSize());
         setVisible(true);
     }
 }
--- com/NetlabUT/ThrowableDetail.java
+++ com/NetlabUT/ThrowableDetail.java
@@ -1,12 +1,48 @@
 package com.NetlabUT;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
 
 public class ThrowableDetail extends JFrame
 {
+    private JTextPane textMessage;
+    private JList<String> listStackTrace;
+    private JLabel textType;
+    private JPanel mainPanel;
+
+    private Throwable throwable;
+
     public ThrowableDetail(Object object)
     {
+        if (object instanceof Throwable == false)
+            return;
+
+        this.throwable = (Throwable) object;
+
+        textType.setText(throwable.getClass().getName());
+        textMessage.setText(throwable.getMessage());
+
+        DefaultListModel<String> model = new DefaultListModel<>();
+        listStackTrace.setModel(model);
+
+        StackTraceElement[] stackTrace = throwable.getStackTrace();
+        for (StackTraceElement element : stackTrace)
+            model.addElement(element.toString());
+
+        listStackTrace.addListSelectionListener(this::onStackTraceListSelected);
+
         setTitle(Profile.getObjectIdentifierString(object));
+        setContentPane(mainPanel);
+        setMinimumSize(mainPanel.getMinimumSize());
+        setSize(mainPanel.getMinimumSize());
         setVisible(true);
+    }
+
+    private void onStackTraceListSelected(ListSelectionEvent e)
+    {
+        int idx = listStackTrace.getSelectedIndex();
+        if (idx == -1) return;
+        StackTraceElement trace = throwable.getStackTrace()[idx];
+        System.err.println(trace.toString());
     }
 }
