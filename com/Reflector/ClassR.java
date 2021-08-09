ea08a8ff5d59971f66e16abf32a4343be880e125
ramdanks <ramadhan.kalih@ui.ac.id>
2021-08-09 22:51:41 +0700

    catch Throwable to List

--- com/Reflector/ClassR.java
+++ com/Reflector/ClassR.java
@@ -2,6 +2,8 @@ package com.Reflector;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /** Class type wrapper to suppress throwable
  * @author Ramadhan Kalih Sewu
@@ -10,6 +12,7 @@ import java.lang.reflect.Method;
 public class ClassR
 {
     private Class<?> mClass;
+    private ArrayList<Throwable> mThrowableList = new ArrayList<>();
 
     public ClassR(Class<?> aClass) { mClass = aClass; }
     public ClassR (String packageName, String className)
@@ -18,72 +21,84 @@ public class ClassR
             if (packageName == null || packageName.isEmpty())
                 mClass = Class.forName(className);
             mClass = Class.forName(packageName + '.' + className); }
-        catch (Throwable ignored) {}
+        catch (Throwable t) { mThrowableList.add(t); }
     }
     public ClassR(String className)
     {
         try { mClass = Class.forName(className); }
-        catch (Throwable ignored) {}
+        catch (Throwable t) { mThrowableList.add(t); }
     }
 
     public Class<?> getContainingClass() { return mClass; }
+
+    public ArrayList<Throwable> getThrowableList() { return mThrowableList; }
 
     /** create an instance of a class using default constructor */
     public Object newInstance()
     {
         try { return mClass.getConstructor().newInstance(); }
-        catch (Throwable ignored) {}
+        catch (Throwable t) { mThrowableList.add(t); }
         return null;
     }
 
     public Object newInstance(Constructor<?> constructor, Object... args)
     {
         try { return constructor.newInstance(args); }
-        catch (Throwable ignored) {}
+        catch (Throwable t) { mThrowableList.add(t); }
         return null;
     }
 
     public Constructor<?> getConstructor(Class<?>... paramTypes)
     {
         try { return mClass.getConstructor(paramTypes); }
-        catch (Throwable ignored) {}
+        catch (Throwable t) { mThrowableList.add(t); }
         return null;
     }
 
     public Method getMethod(String funcName, Class<?>... paramTypes)
     {
         try { return mClass.getMethod(funcName, paramTypes); }
-        catch (Throwable ignored) {}
+        catch (Throwable t) { mThrowableList.add(t); }
         return null;
     }
 
     public Field getField(String name)
     {
         try { return mClass.getField(name); }
-        catch (Throwable ignored) {}
+        catch (Throwable t) { mThrowableList.add(t); }
         return null;
     }
 
     public Field getDeclaredField(String name)
     {
         try { return mClass.getDeclaredField(name); }
-        catch (Throwable ignored) {}
+        catch (Throwable t) { mThrowableList.add(t); }
         return null;
     }
 
     public boolean setField(Field field, Object object, Object value)
     {
         try { field.set(object, value); }
-        catch (Throwable ignored) { return false; }
+        catch (Throwable t)
+        {
+            mThrowableList.add(t);
+            return false;
+        }
         return true;
     }
 
     public boolean setFieldForce(Field field, Object object, Object value)
     {
-        try {
+        try
+        {
             field.setAccessible(true);
             field.set(object, value);
-        } catch (Throwable ignored) { return false; }
+        }
+        catch (Throwable t)
+        {
+            mThrowableList.add(t);
+            return false;
+        }
         return true;
     }
 
