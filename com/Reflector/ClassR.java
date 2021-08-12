eef765d03d158e880ca3aa86d482562e828fcd8a
ramdanks <ramadhan.kalih@ui.ac.id>
2021-08-12 11:52:12 +0700

    add jdocs

--- com/Reflector/ClassR.java
+++ com/Reflector/ClassR.java
@@ -14,7 +14,10 @@ public class ClassR
     private Class<?> mClass;
     private ArrayList<Throwable> mThrowableList = new ArrayList<>();
 
+    /** assign containing class */
     public ClassR(Class<?> aClass) { mClass = aClass; }
+    /** find a requested class from {@link Class#forName(String)}. This will concatenate {@code packageName}
+     * with {@code className} with '.' if packageName is not {@code null} */
     public ClassR (String packageName, String className)
     {
         try
@@ -25,14 +28,17 @@ public class ClassR
         }
         catch (Throwable t) { mThrowableList.add(t); }
     }
+    /** find a requested class from {@link Class#forName(String)} */
     public ClassR(String className)
     {
         try { mClass = Class.forName(className); }
         catch (Throwable t) { mThrowableList.add(t); }
     }
 
+    /** get Class that is contained in this */
     public Class<?> getContainingClass() { return mClass; }
 
+    /** get every throwable that happens when using a method from {@link ClassR} */
     public ArrayList<Throwable> getThrowableList() { return mThrowableList; }
 
     /** create an instance of a class using default constructor */
@@ -43,6 +49,9 @@ public class ClassR
         return null;
     }
 
+    /** create an instance from specified constructor
+     * @return Object constructed from the {@code constructor} or
+     * null if {@code constructor} is null or {@code args} doesn't satisfy */
     public Object newInstance(Constructor<?> constructor, Object... args)
     {
         try { return constructor.newInstance(args); }
@@ -50,6 +59,8 @@ public class ClassR
         return null;
     }
 
+    /** find a constructor using {@link Class#getConstructor(Class[])}
+     * @return Constructor that is requested or null if not found or experience Throwable */
     public Constructor<?> getConstructor(Class<?>... paramTypes)
     {
         try { return mClass.getConstructor(paramTypes); }
@@ -57,6 +68,8 @@ public class ClassR
         return null;
     }
 
+    /** find a method using {@link Class#getMethod(String, Class[])}
+     * @return Method that is requested or null if not found or experience Throwable */
     public Method getMethod(String funcName, Class<?>... paramTypes)
     {
         try { return mClass.getMethod(funcName, paramTypes); }
@@ -64,6 +77,8 @@ public class ClassR
         return null;
     }
 
+    /** find a method using {@link Class#getDeclaredMethod(String, Class[])}
+     * @return Method that is requested or null if not found or experience Throwable */
     public Method getDeclaredMethod(String funcName, Class<?>... paramTypes)
     {
         try { return mClass.getDeclaredMethod(funcName, paramTypes); }
@@ -71,6 +86,8 @@ public class ClassR
         return null;
     }
 
+    /** find a field using {@link Class#getField(String)}
+     * @return Field that is requested or null if not found or experience Throwable */
     public Field getField(String name)
     {
         try { return mClass.getField(name); }
@@ -78,6 +95,8 @@ public class ClassR
         return null;
     }
 
+    /** find a field using {@link Class#getDeclaredField(String)}
+     * @return Field that is requested or null if not found or experience Throwable */
     public Field getDeclaredField(String name)
     {
         try { return mClass.getDeclaredField(name); }
@@ -85,6 +104,8 @@ public class ClassR
         return null;
     }
 
+    /** set field using {@link Field#set(Object, Object)}
+     * @return true if success false otherwise */
     public boolean setField(Field field, Object object, Object value)
     {
         try { field.set(object, value); }
@@ -96,6 +117,9 @@ public class ClassR
         return true;
     }
 
+    /** set field using {@link Field#set(Object, Object)}. This will also make field accessible
+     * for the future by using {@link Field#setAccessible(boolean)}.
+     * @return true if success false otherwise */
     public boolean setFieldForce(Field field, Object object, Object value)
     {
         try
